1d7c1529b17646c2e982bfc8a7c18f8a22ed9d71
Xeo Skr <skypt444@gmail.com>
2020-05-05 11:09:24 +0900

    fix field

--- src/Dessert/GraphQL/Mutation.cs
+++ src/Dessert/GraphQL/Mutation.cs
@@ -197,7 +197,7 @@ namespace Dessert.GraphQL
             var input = context.Argument<Account>("account");
             var account = await userManager.GetUserAsync(context.GetClaimsPrincipal());
 
-            account.Nickname = input.UserName;
+            account.Nickname = input.Nickname;
             account.ProfilePicUrl = input.ProfilePicUrl;
 
             var result = await userManager.UpdateAsync(account);
